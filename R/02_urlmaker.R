08750bbe9ce446f52fe8974387769a6f82e54172
lottawagner <lotta.wagner@wagbi.de>
2024-07-22 15:00:33 +0200

    created the first url maker for stringdb

--- R/02_urlmaker.R
+++ R/02_urlmaker.R
@@ -1,32 +1,74 @@
+# STRINGDB -----
 
+## stringdb species -----
 
+#' info_species_stringdb
+#'
+#' @param version #current version of the data files in stringdb
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
 
+info_species_stringdb <- function(version = "12.0"){
 
+  # use sprintf function to insert current version into the url
+  url_species_stringdb <- sprintf("https://stringdb-downloads.org/download/species.v%s.txt", version)
 
-urlmaker_stringdb <- function(set = c("", "", ""),
-                               organism = "Human",
-                               remap.uniprot.ids = FALSE,
-                               cache = TRUE,
-                               mode = c("ehub", "web")) {
+  # read.delim the data from the species text file (columns separated using a delimiter)
+  df_species <- read.delim(url(url_species_stringdb))
 
-
-# link: https://stringdb-downloads.org/download/protein.links.v12.0/9606.protein.links.v12.0.txt.gz
+  # return the datafile to the caller
+  return(df_species)
 }
 
 
-urlmaker_intact <- function() {
+## stringdb urlmaker -----
 
+#' urlmaker_stringdb
+#'
+#' @param type # type of information that is stored in the file
+#' @param species # from which species does the data come from
+#' @param version
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
 
-# link: /Users/mac/R/2024_Master_IMBEI/thesisproject_networks/allPPIdbs/IntAct/Human1.zip (ZIP!!)
+urlmaker_stringdb <- function(type = "PPI", # or protein info for stringdb
+                              species = "Homo sapiens",
+                              version = "12.0") {
 
+  # "https://stringdb-downloads.org/download/protein.links.v12.0/9606.protein.links.v12.0.txt.gz"
 
+  #match the information about the species with the corresponding data file (PPI/protein info)
+
+  info_species <- info_stringdb_species(version = version)
+  species_id <- info_species$X.taxon_id[
+    match(species, info_species$official_name_NCBI)
+  ]
+
+  if (type == "PPI") {
+    url <- sprintf(
+      "https://stringdb-downloads.org/download/protein.links.v%s/%s.protein.links.v%s.txt.gz",
+      version,
+      species_id,
+      version
+    )
+  } else if (type == "protein_info") {
+    url <- sprintf(
+      "https://stringdb-downloads.org/download/protein.info.v%s/%s.protein.info.v%s.txt.gz",
+      version,
+      species_id,
+      version
+    )
+  }
+
+  return(url)
 }
 
 
-urlmaker_biogrid <- function() {
-
-
-# link: https://downloads.thebiogrid.org/Download/BioGRID/Release-Archive/BIOGRID-4.4.233/BIOGRID-ALL-4.4.233.mitab.zip
-
-
-}
