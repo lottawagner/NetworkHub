fd431d048c6ca2604bade3c4b33378fefbaf53a4
lottawagner <lotta.wagner@wagbi.de>
2024-08-01 16:09:44 +0200

    created a file to get_networkdata for corum

---
+++ R/03-4_db_corum.R
@@ -0,0 +1,13 @@
+# here we need:
+      #function to define the organism inside the file
+      #function to define each protein of a complex as a interaction unit
+      #function to get annotation info
+      # function to build the graph
+
+info_species_corum <- function(species,
+                               cache = TRUE )
+
+
+  species <- list("Human", "Mouse", "Pig", "Bovine", "Rat", "Mammalia", "Rabbit", "Dog", "Hamster", "MINK")
+
+
