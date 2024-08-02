903a6fdc6bd6d6e843d177ef8ede196bcb9087d1
lottawagner <lotta.wagner@wagbi.de>
2024-08-02 14:50:30 +0200

    worked on the sticker

--- inst/scripts/04_NetworkHub_sticker.R
+++ inst/scripts/04_NetworkHub_sticker.R
@@ -1,19 +1,34 @@
 # Creation of the NetworkHub sticker using HexSticker
 
-#install.packages("hexSticker")
-#install.packages("magick")
-#install.packages("sysfonts")
-#install.packages("tidyverse")
+if (!requireNamespace("hexSticker", quietly = TRUE))
+  install.packages("hexSticker")
+if (!requireNamespace("magick", quietly = TRUE))
+  install.packages("magick")
+if (!requireNamespace("sysfonts", quietly = TRUE))
+  install.packages("sysfonts")
+if (!requireNamespace("tidyverse", quietly = TRUE))
+  install.packages("tidyverse")
 
 library(hexSticker)
 library(magick)
 library(sysfonts)
 library(tidyverse)
 
-nwh_img <- image_read("netzwerk.png")
 
-sticker(
-  subplot = nwh_img,
-  package = "NetworkHub"
-) %>%print(sticker())
+
+nwh_img <- "/Users/mac/R/2024_Master_IMBEI/NetworkHub/inst/scripts/netzwerk.png"
+
+
+sticker(subplot = nwh_img,
+  package = "NetworkHub",
+  s_width = 1,
+  s_height = 1,
+  s_x = 1,
+  s_y = 0.75
+) %>% print(sticker())
+
+
+
+
+
 
