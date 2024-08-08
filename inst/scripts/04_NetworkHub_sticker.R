d76e69c6a489d41f32035878eb6d835b5be875ed
lottawagner <lotta.wagner@wagbi.de>
2024-08-08 15:26:22 +0200

    sticker_option_orangetext

--- inst/scripts/04_NetworkHub_sticker.R
+++ inst/scripts/04_NetworkHub_sticker.R
@@ -16,21 +16,21 @@ library(tidyverse)
 
 
 setwd("/Users/mac/R/2024_Master_IMBEI/NetworkHub/inst/scripts")
-nwh_img <- image_read("N_blue.png")
+nwh_img <- image_read("N_orange.png")
 
 sticker(subplot = nwh_img,
   package = "etworkHub",
-  s_width = 0.9,
-  s_height = 0.9,
-  s_x = 1.1,
-  s_y = 0.5,
-  p_x = 0.2,
+  s_width = 0.65,
+  s_height = 0.65,
+  s_x = 0.45,
+  s_y = 1,
+  p_x = 1.275,
   p_y = 1,
-  p_color = "darkslateblue",
-  p_size = 22,
+  p_color = "tomato1",
+  p_size = 17,
   h_size = 1.6,
-  h_fill = "darkseagreen",
-  h_color = "lemonchiffon") %>% print(sticker())
+  h_fill = "slateblue3",
+  h_color = "white") %>% print(sticker())
 
 
 
