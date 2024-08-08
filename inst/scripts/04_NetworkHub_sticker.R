ed444d1d45bd2c693154da122e99ab2133db3d76
lottawagner <lotta.wagner@wagbi.de>
2024-08-08 16:10:17 +0200

    defined the resultion of the image

--- inst/scripts/04_NetworkHub_sticker.R
+++ inst/scripts/04_NetworkHub_sticker.R
@@ -18,6 +18,7 @@ library(tidyverse)
 setwd("/Users/mac/R/2024_Master_IMBEI/NetworkHub/inst/scripts")
 nwh_img <- image_read("N_blue.png")
 
+
 sticker(subplot = nwh_img,
   package = "etworkHub",
   s_width = 0.65,
@@ -27,13 +28,14 @@ sticker(subplot = nwh_img,
   p_x = 1.275,
   p_y = 1,
   p_color = "blue",
-  p_size = 17,
+  p_size = 68,
   h_size = 1.6,
   h_fill = "tomato1",
   h_color = "#E5E5E5",
   url = "https://github.com/lottawagner/NetworkHub",
-  u_size = 3.5,
-  u_color = "white") %>% print(sticker())
+  u_size = 14,
+  u_color = "white",
+  dpi = 1200) %>% print(sticker())
 
 
 
