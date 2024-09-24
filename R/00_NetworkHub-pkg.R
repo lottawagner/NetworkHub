2d08d2f6382e093eb05db99a2a21c8b76138a2ab
lottawagner <lotta.wagner@wagbi.de>
2024-09-24 15:48:15 +0200

    cmd+shift+e restructure roxygen sceleton , ...

--- R/00_NetworkHub-pkg.R
+++ R/00_NetworkHub-pkg.R
@@ -8,3 +8,29 @@
 #' @docType package
 #' @keywords internal
 "_PACKAGE"
+
+# required_pkgs_nwh <- c("BiocFileCache",
+#                        "dplyr",
+#                        "vroom",
+#                        "AnnotationDbi",
+#                        "org.At.tair.db",
+#                        "org.Bt.eg.db",
+#                        "org.Ce.eg.db",
+#                        "org.Cf.eg.db",
+#                        "org.Dm.eg.db",
+#                        "org.Dr.eg.db",
+#                        "org.EcK12.eg.db",
+#                        "org.Gg.eg.db",
+#                        "org.Hs.eg.db",
+#                        "org.Mm.eg.db",
+#                        "org.Pf.plasmo.db",
+#                        "org.Rn.eg.db",
+#                        "org.Sc.sgd.db",
+#                        "org.Ss.eg.db")
+# BiocManager::install(required_pkgs_nwh)
+
+#TODO force = TRUE ?
+
+# working ON NetworkHub:
+# rm(list = ls())
+# devtools::load_all(".")
