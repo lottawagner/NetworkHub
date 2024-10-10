7f10017344c764479a165d788c0b511e70b8b380
lottawagner <lotta.wagner@wagbi.de>
2024-10-10 12:28:54 +0200

    reactome: get_networkdata() + annotation() done, selected 11 "core species" and annotated via AnnotationDbi

--- R/03-12_db_reactome.R
+++ R/03-12_db_reactome.R
@@ -4,7 +4,7 @@
 
 #' get_networkdata_reactome()
 #'
-#' @param species  from which species does the data come from
+#' @param species  default value = "taxid:9606(Homo sapiens)" - from which species does the data come from
 #' @param version version of the data files in reactome
 #' @param cache default value set to TRUE (automatically checks if the data file is already stored in the cache)
 #' @param add_annotation expanding the dataframe with four columns (Entrez_ID and Ensembl_ID)
@@ -16,16 +16,13 @@
 #' @export
 #'
 #' @examples
-#'
-#' db_reactome_df <- get_networkdata_reactome(
-#'   species = "human",
-#'   version = "2021-05"
-#' )
-#'
+#' db_reactome_df <- get_networkdata_reactome(species = "taxid:9606(Homo sapiens)",
+#'                                            version = "current"
+#'                                            )
 #' db_reactome_df
 #'
 
-get_networkdata_reactome <- function(species,
+get_networkdata_reactome <- function(species = "taxid:9606(Homo sapiens)",
                                      version = "current",
                                      cache = TRUE,
                                      add_annotation = TRUE,
@@ -34,11 +31,19 @@ get_networkdata_reactome <- function(species,
   # list species is actualized for version reactome "current" (sept 2024)
   # UPDATEVERSION
 
+  # check that the value for species is listed in reactome
+
+  if (!(species %in% list_species_reactome)) { # if species is not in the list
+    stop("Species not found as specified by reactome,",
+         "please check some valid entries by running `list_species_reactome`") # stop function and print
+  }
+
+
   # buildup of the resource location for the version and all
   ## elegantly done in another smaller utility function
 
   rname <- paste0(
-    "reactome_v",
+    "reactome_v_",
     version,
     "_",
     species
@@ -69,83 +74,177 @@ get_networkdata_reactome <- function(species,
   # read in the resource, whether cached or freshly downloaded
   ppis_reactome <- vroom::vroom(network_file)
   #ppis_reactome <- head(read.delim(network_file, sep = " "))
-  #
-  message(dim(ppis_reactome))
+
+  # If you want to check all species contained in the file
+  #species_A <- unique(ppis_reactome$`Taxid interactor A`)
+  #species_B <- unique(ppis_reactome$`Taxid interactor B`)
+  #info_species_reactome <- union(species_A, species_B)
+
+
+  #Filter for species
+
+  ppis_reactome_filtered <- ppis_reactome[(ppis_reactome$`Taxid interactor A` == species ) &
+                                            (ppis_reactome$`Taxid interactor B` == species ),
+  ]
 
   # rename columns
 
   #Uniprot
-  colnames(ppis_reactome)[colnames(ppis_reactome) == "#ID(s) interactor A"] <- "Uniprot_A"
-  colnames(ppis_reactome)[colnames(ppis_reactome) == "ID(s) interactor B"] <- "Uniprot_B"
+  colnames(ppis_reactome_filtered)[colnames(ppis_reactome_filtered) == "#ID(s) interactor A"] <- "Uniprot_A"
+  colnames(ppis_reactome_filtered)[colnames(ppis_reactome_filtered) == "ID(s) interactor B"] <- "Uniprot_B"
 
-  ppis_reactome_filtered <- ppis_reactome[(ppis_reactome$`Taxid interactor A` == species | ppis_reactome$`Taxid interactor A` == "-") &
-                                          (ppis_reactome$`Taxid interactor B` == species | ppis_reactome$`Taxid interactor B` == "-"),
-                                          ]
+  #extract Uniprot_id
+  ppis_reactome_filtered$Uniprot_A <- str_extract(ppis_reactome_filtered$Uniprot_A, "uniprotkb:([A-Z0-9]+)")
+  ppis_reactome_filtered$Uniprot_A <- gsub("uniprotkb:", "", ppis_reactome_filtered$Uniprot_A)
+
+  ppis_reactome_filtered$Uniprot_B <- str_extract(ppis_reactome_filtered$Uniprot_B, "uniprotkb:([A-Z0-9]+)")
+  ppis_reactome_filtered$Uniprot_B <- gsub("uniprotkb:", "", ppis_reactome_filtered$Uniprot_B)
+
+
 
   if (add_annotation) {
 
-
-    url_reactome_annotation_info <- "https://reactome.org/download/current/interactors/reactome.all_species.interactions.tab-delimited.txt"
-
-    rname <- paste0(
-      "reactome_",
-      version,
-      "_",
-      species,
-      "_annotation_info"
-      )
-
-    if (cache) {
-      # tries to fetch from the cache
-      message("Trying to fetch from cache...")
-      network_file_annotation <- fetch_NetworkHub(rname)
-    }
-
-    if (!cache | is.null(network_file_annotation)) {
-      # retrieves the file for the first time
-      message("Downloading to cache...")
-      network_file_annotation <- cache_NetworkHub(
-        rname = rname,
-        fpath = url_reactome_annotation_info
-      )
-    }
-
-    reactome_annotation_info <- vroom::vroom(network_file_annotation)
-
-    #rename columns Uniprot
-    colnames(reactome_annotation_info)[colnames(reactome_annotation_info) == "# Interactor 1 uniprot id"] <- "Uniprot_A"
-    colnames(reactome_annotation_info)[colnames(reactome_annotation_info) == "Interactor 2 uniprot id"] <- "Uniprot_B"
-
-    #merge dataframes into new dataframe
-    ppis_reactome_filtered_annotated <- merge(ppis_reactome_filtered, reactome_annotation_info, by = c("Uniprot_A", "Uniprot_B"), all.x = TRUE)
-    ppis_reactome_filtered_annotated <- ppis_reactome_filtered_annotated[
-      !(ppis_reactome_filtered_annotated$`Taxid interactor A` == "-" &
-          ppis_reactome_filtered_annotated$`Taxid interactor B` == "-"),
-    ]
-
-    #rename columns Entrez
-    colnames(ppis_reactome_filtered_annotated)[colnames(ppis_reactome_filtered_annotated) == "Interactor 1 Entrez Gene id"] <- "Entrez_A"
-    colnames(ppis_reactome_filtered_annotated)[colnames(ppis_reactome_filtered_annotated) == "Interactor 2 Entrez Gene id"] <- "Entrez_B"
-
-    #rename columns Ensembl
-    colnames(ppis_reactome_filtered_annotated)[colnames(ppis_reactome_filtered_annotated) == "Interactor 1 Ensembl gene id"] <- "Ensembl_A"
-    colnames(ppis_reactome_filtered_annotated)[colnames(ppis_reactome_filtered_annotated) == "Interactor 2 Ensembl gene id"] <- "Ensembl_B"
-
-    #extract UniProt_id
-    ppis_reactome_filtered_annotated$Uniprot_A <- str_extract(ppis_reactome_filtered_annotated$Uniprot_A, "uniprotkb:([A-Z0-9]+)")
-    ppis_reactome_filtered_annotated$Uniprot_A <- gsub("uniprotkb:", "", ppis_reactome_filtered_annotated$Uniprot_A)
-
-    ppis_reactome_filtered_annotated$Uniprot_B <- str_extract(ppis_reactome_filtered_annotated$Uniprot_B, "uniprotkb:([A-Z0-9]+)")
-    ppis_reactome_filtered_annotated$Uniprot_B <- gsub("uniprotkb:", "", ppis_reactome_filtered_annotated$Uniprot_B)
-
-
-    return(ppis_reactome_filtered_annotated)
+    ppi_reactome_filtered_annotated <- annotation_reactome(ppi_reactome = ppis_reactome_filtered,
+                                                           species = species,
+                                                           version = version)
+    return(ppi_reactome_filtered_annotated)
   }
 
   if (!add_annotation) {
     return(ppis_reactome_filtered)
   }
 }
+
+
+# Outside function ----------
+
+
+#SPECIES NetworkHub uses from reactome
+list_species_reactome <- c("taxid:9913(Bos taurus)",
+                           "taxid:6239(Caenorhabditis elegans)",
+                           "taxid:9615(Canis familiaris)",
+                           "taxid:7227(Drosophila melanogaster)",
+                           "taxid:562(Escherichia coli)",
+                           "taxid:9031(Gallus gallus)",
+                           "taxid:9606(Homo sapiens)",
+                           "taxid:10090(Mus musculus)",
+                           "taxid:10116(Rattus norvegicus",
+                           "taxid:4932(Saccharomyces cerevisiae)",
+                           "taxid:8355(Xenopus laevis)"
+                          )
+
+
+list_db_annotationdbi_reactome <- c("org.Bt.eg.db",
+                                   "org.Ce.eg.db",
+                                   "org.Cf.eg.db",
+                                   "org.Dm.eg.db",
+                                   "org.EcK12.eg.db",
+                                   "org.Gg.eg.db",
+                                   "org.Hs.eg.db",
+                                   "org.Mm.eg.db",
+                                   "org.Rn.eg.db",
+                                   "org.Sc.sgd.db",
+                                   "org.Xl.eg.db"
+                                   )
+
+
+reactome_db_annotations <- data.frame(species = list_species_reactome,
+                                 anno_db_reactome = list_db_annotationdbi_reactome,
+                                 row.names = list_species_reactome
+)
+
+
+# annotation_reactome() --------
+
+#' annotation_reactome ()
+#'
+#' @param species  from which species does the data come from
+#' @param version version of the data files in reactome
+#' @param ppi_reactome variable defined by ppis_reactome in get_networkdata_reactome()
+#'
+#' @importFrom AnnotationDbi mapIds
+#' @import org.Bt.eg.db
+#' @import org.Ce.eg.db
+#' @import org.Cf.eg.db
+#' @import org.Dm.eg.db
+#' @import org.EcK12.eg.db
+#' @import org.Gg.eg.db
+#' @import org.Hs.eg.db
+#' @import org.Mm.eg.db
+#' @import org.Rn.eg.db
+#' @import org.Sc.sgd.db
+#' @import org.Xl.eg.db
+#'
+#'@return ppis_reactome_annotated
+#'
+#'@export
+#'
+#'
+#' @examples
+#'
+#' # annotation_reactome(ppi_reactome, species = "taxid:9606(Homo sapiens)", version = "current")
+
+annotation_reactome <- function(ppi_reactome,
+                                species,
+                                version)
+                                {
+  # find database on corresponding species
+
+  if (!(species %in% list_species_reactome)) { # if species is not in the list
+    stop("Species not found as specified by reactome,",
+         "please check some valid entries by running `list_species_reactome`") # stop function and print
+  }
+
+  annotation_db <-
+    reactome_db_annotations$anno_db_reactome[match(species, reactome_db_annotations$species)]
+
+
+  all_prot_ids <- unique(na.omit(c(ppi_reactome$Uniprot_A, ppi_reactome$Uniprot_B)))
+
+  anno_df <- data.frame(uniprot_id = all_prot_ids,
+                        gene_symbol <- mapIds(get(annotation_db),
+                                              keys = all_prot_ids,
+                                              keytype = "UNIPROT",
+                                              column = "SYMBOL"
+                                              ),
+                        ensembl_id <- mapIds(get(annotation_db),
+                                              keys = all_prot_ids,
+                                              keytype = "UNIPROT",
+                                              column = "ENSEMBL"
+                                            ),
+                        entrez_ids <- mapIds(get(annotation_db),
+                                             keys = all_prot_ids,
+                                             keytype = "UNIPROT",
+                                             column = "ENTREZID"
+                                            ),
+                        row.names = all_prot_ids
+                        )
+
+  ppis_reactome_annotated <- ppi_reactome
+
+  #adding GeneSymbol
+  ppis_reactome_annotated$GeneSymbol_A <-
+    anno_df$gene_symbol[match(ppis_reactome_annotated$Uniprot_A, anno_df$uniprot_id)]
+  ppis_reactome_annotated$GeneSymbol_B <-
+    anno_df$gene_symbol[match(ppis_reactome_annotated$Uniprot_B, anno_df$uniprot_id)]
+
+  #adding Ensembl
+  ppis_reactome_annotated$Ensembl_A <-
+    anno_df$ensembl_id[match(ppis_reactome_annotated$Uniprot_A, anno_df$uniprot_id)]
+  ppis_reactome_annotated$Ensembl_B <-
+    anno_df$ensembl_id[match(ppis_reactome_annotated$Uniprot_B, anno_df$uniprot_id)]
+
+  #adding Entrez
+  ppis_reactome_annotated$Entrez_A <-
+    anno_df$entrez_id[match(ppis_reactome_annotated$Uniprot_A, anno_df$uniprot_id)]
+  ppis_reactome_annotated$Entrez_B <-
+    anno_df$entrez_id[match(ppis_reactome_annotated$Uniprot_B, anno_df$uniprot_id)]
+
+  return(ppis_reactome_annotated)
+}
+
+
+
 
 
 
