02972ed8930a0d2c87c978bede24c82eab46ef1e
lottawagner <lotta.wagner@wagbi.de>
2024-10-14 12:54:10 +0200

    hint: added build_graph_hint()

--- R/03-2_db_hint.R
+++ R/03-2_db_hint.R
@@ -16,7 +16,7 @@
 #' @export
 #'
 #' @examples
-#' \donttest{
+#' \dontrun{
 #' db_hint_df <- get_networkdata_hint(
 #'   species = "HomoSapiens",
 #'   version = "2024-06",
@@ -173,7 +173,7 @@ hint_db_annotations <- data.frame(species = list_species_hint,
 #'
 #'
 #' @examples
-#' #\donttest{
+#' #\dontrun{
 #' # annotation_hint <- annotation_hint(ppi_hint, species = "HomoSapiens", version = "2024-06", type = "binary")
 #' # annotation_hint
 #' #}
@@ -236,5 +236,80 @@ annotation_hint <- function(ppi_hint,
 
 
 
+
+
+
+# build_graph_hint() -----
+
+#' Title
+#'
+#' @param graph_data ppi data from HINT
+#' @param output_format selection of different graph functions that can be used
+#' @param min_score_treshold select ppis that are "confident" depending on the scoretype/value
+#'
+#' @import igraph
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' db_hint_df <- get_networkdata_hint(
+#'   species = "HomoSapiens",
+#'   version = "2024-06",
+#'   type = "binary"
+#' )
+#'
+#' db_hint_graph <- build_graph_hint(graph_data = db_hint_df,
+#'                                  output_format = "igraph",
+#'                                  min_score_treshold = TRUE)
+#' db_hint_graph
+#' }
+#' db_hint_graph_plot <- ggplot(db_hint_graph)
+#'
+
+
+build_graph_hint <- function (graph_data,
+                              output_format = "igraph",
+                              min_score_treshold = TRUE
+                              ){
+
+  #check on the clumns in your ppi data file
+  colnames(graph_data)
+
+
+  #select ppi data >= minimal score
+  if (min_score_treshold) {
+    graph_data_processed <- graph_data[graph_data$high_quality, ]
+  }
+
+  #select all ppi data
+  else {
+    graph_data_processed <- graph_data
+  }
+
+  #check on dimension (amount of rows)
+  dim(graph_data)
+  dim(graph_data_processed)
+
+  edges <- data.frame(from = graph_data_processed$GeneSymbol_A,
+                      to = graph_data_processed$GeneSymbol_B)
+
+  # Create unique nodes (combine both GeneSymbol columns)
+  nodes <- data.frame(id = unique(c(graph_data_processed$GeneSymbol_A,
+                                    graph_data_processed$GeneSymbol_B)),
+                      label = unique(c(graph_data_processed$GeneSymbol_A,
+                                       graph_data_processed$GeneSymbol_B)))
+
+  # If output format is igraph, return the igraph object
+  if (output_format == "igraph") {
+    whole_graph <- igraph::graph.data.frame(d = edges, directed = FALSE)
+    my_graph <- igraph::simplify(whole_graph)
+    return(my_graph)
+  }
+  # simplify by avoiding multiple entries?
+  ## could make it smaller and easier to handle, without losing too much/at all in info
+
+}
 
 
