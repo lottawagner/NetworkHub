df82c7f7ed28737909d553d2dd0d5358aef4a3a3
lottawagner <lotta.wagner@wagbi.de>
2024-10-15 10:02:35 +0200

    matrixdb: build_graph_matrixdb()

--- R/03-10_db_matrixdb.R
+++ R/03-10_db_matrixdb.R
@@ -198,3 +198,64 @@ annotation_matrixdb <- function(ppi_matrixdb,
   return(ppis_matrixdb_annotated)
 
 }
+
+# build_graph_matrixdb() -----
+
+#' build_graph_matrixdb()
+#'
+#' @param graph_data ppi data from matrixdb
+#' @param output_format selection of different graph functions that can be used
+#' @param min_score_treshold select ppis that are "confident" depending on the scoretype/value
+#'
+#' @import igraph
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'
+#' db_matrixdb_df <- get_networkdata_matrixdb(
+#'   species = "human",
+#'   type = "CORE"
+#' )
+#'
+#' db_matrixdb_graph <- build_graph_matrixdb(graph_data = db_matrixdb_df,
+#'                                         output_format = "igraph")
+#' db_matrixdb_graph #list of 235
+#' }
+#'
+#'
+
+
+build_graph_matrixdb <- function (graph_data,
+                                   output_format = "igraph"){
+
+  #check on the clumns in your ppi data file
+  colnames(graph_data)
+
+  graph_data_processed <- graph_data
+
+  dim(graph_data_processed)
+
+  edges <- data.frame(from = graph_data_processed$GeneSymbol_A,
+                      to = graph_data_processed$GeneSymbol_B)
+
+  # Create unique nodes (combine both GeneSymbol columns)
+  nodes <- data.frame(id = unique(c(graph_data_processed$GeneSymbol_A,
+                                    graph_data_processed$GeneSymbol_B)),
+                      label = unique(c(graph_data_processed$GeneSymbol_A,
+                                       graph_data_processed$GeneSymbol_B)))
+
+  # If output format is igraph, return the igraph object
+  if (output_format == "igraph") {
+    whole_graph <- igraph::graph.data.frame(d = edges, directed = FALSE)
+    my_graph <- igraph::simplify(whole_graph)
+    return(my_graph)
+  }
+  # simplify by avoiding multiple entries?
+  ## could make it smaller and easier to handle, without losing too much/at all in info
+
+}
+
+
