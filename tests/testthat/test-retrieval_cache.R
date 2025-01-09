3b83b898417d49421a85af42ee76170dd44db231
Federico Marini <marinif@uni-mainz.de>
2025-01-09 19:10:43 +0100

    a tiny bit of tests more on the caching mechanism

--- tests/testthat/test-retrieval_cache.R
+++ tests/testthat/test-retrieval_cache.R
@@ -5,4 +5,11 @@ test_that("Cache is correctly initialized", {
   expect_s4_class(bfc_nh, "BiocFileCache")
   expect_true(BiocFileCache::bfccount(bfc_nh) == 0)
 
+  bfc_nh
+
+  cpdb_url <- urlmaker_cpdb(species = "human")
+  test_rpath <- BiocFileCache::bfcadd(x = bfc_nh, rname = "cpdbtest", cpdb_url)
+
+  expect_true(BiocFileCache::bfccount(bfc_nh) == 1)
+  expect_type(test_rpath, "character")
 })
